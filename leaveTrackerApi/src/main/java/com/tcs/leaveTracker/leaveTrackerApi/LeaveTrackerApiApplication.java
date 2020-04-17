e4d89e8b7e65dce8e442ad09723f141d2c9afaa8
Dishant Jani <dishant.jani@tcs.com>
2020-04-17 20:09:39 +0530

    Initial project structure for backend Application

---
+++ leaveTrackerApi/src/main/java/com/tcs/leaveTracker/leaveTrackerApi/LeaveTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.leaveTracker.leaveTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveTrackerApiApplication.class, args);
+	}
+
+}
